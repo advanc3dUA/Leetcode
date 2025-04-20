80c254a2ca22edf04d3d92906602111e360b12da
Yuriy Gudimov <gudimovyuriy@gmail.com>
2025-04-20 10:08:32 +0200

    Add solution for 11. Container With Most Water

--- TwoPointers/11. Container With Most Water.playground/Contents.swift
+++ TwoPointers/11. Container With Most Water.playground/Contents.swift
@@ -25,6 +25,29 @@ import UIKit
 
 class Solution {
     func maxArea(_ height: [Int]) -> Int {
+        var left = 0
+        var right = height.count - 1
+        var maxArea = 0
         
+        while left < right {
+            let currentArea = (right - left) * min(height[left], height[right])
+            
+            if currentArea > maxArea {
+                maxArea = currentArea
+            }
+            
+            if height[left] < height[right] {
+                left += 1
+            } else {
+                right -= 1
+            }
+        }
+        return maxArea
     }
 }
+
+
+
+let solution = Solution()
+print(solution.maxArea([1,8,6,2,5,4,8,3,7]))
+//print(solution.maxArea([1,2,1]))
