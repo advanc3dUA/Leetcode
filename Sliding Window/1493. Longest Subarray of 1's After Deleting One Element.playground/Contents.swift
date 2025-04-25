bb99875490666d4fed9379ba84f366dc0d29f221
Yuriy Gudimov <gudimovyuriy@gmail.com>
2025-04-25 12:56:36 +0200

    Add solution for 1493. Longest Subarray of 1's After Deleting One Element

--- Sliding Window/1493. Longest Subarray of 1's After Deleting One Element.playground/Contents.swift
+++ Sliding Window/1493. Longest Subarray of 1's After Deleting One Element.playground/Contents.swift
@@ -4,7 +4,7 @@ import UIKit
 //
 //Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
 //
-// 
+//
 //
 //Example 1:
 //
@@ -21,7 +21,7 @@ import UIKit
 //Input: nums = [1,1,1]
 //Output: 2
 //Explanation: You must delete one element.
-// 
+//
 //
 //Constraints:
 //
@@ -30,6 +30,51 @@ import UIKit
 
 class Solution {
     func longestSubarray(_ nums: [Int]) -> Int {
+        var longestSubArrayLength = 0
+        var left = 0
+        var right = 0
+        var nullsCount = 0
+        var firstNullIndex = 0
         
+        for right in 0..<nums.count {
+            if nums[right] == 0 {
+                nullsCount += 1
+                
+                if nullsCount > 1 {
+                    left = firstNullIndex + 1
+                    nullsCount -= 1
+                }
+                
+                firstNullIndex = right
+            }
+            if nullsCount <= 1 {
+                longestSubArrayLength = max(longestSubArrayLength, right - left + 1 - nullsCount)
+                
+            }
+        }
+        return nullsCount > 0 ? longestSubArrayLength : longestSubArrayLength - 1
     }
 }
+
+class Solution2 {
+    func longestSubarray(_ nums: [Int]) -> Int {
+        var lastZero = -1
+        var prevZero = -1
+        var maxLen = 0
+        
+        for i in 0..<nums.count {
+            if nums[i] == 0 {
+                prevZero = lastZero
+                lastZero = i
+            }
+            maxLen = max(maxLen, i - prevZero - 1)
+        }
+        return maxLen
+    }
+}
+
+let solution = Solution()
+print(solution.longestSubarray([1,1,0,1])) // 3
+print(solution.longestSubarray([0,1,1,1,0,1,1,0,1])) // 5
+print(solution.longestSubarray([1,1,1])) // 2
+print(solution.longestSubarray([1,1,0,0,1,1,1,0,1])) // 4
