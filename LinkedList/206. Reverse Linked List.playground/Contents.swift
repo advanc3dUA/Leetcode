c0f187a09226d6fcf6d6fb60776ab251b6775ddf
Yuriy Gudimov <gudimovyuriy@gmail.com>
2025-05-13 10:52:18 +0200

    Add solution for 206. Reverse Linked List

--- LinkedList/206. Reverse Linked List.playground/Contents.swift
+++ LinkedList/206. Reverse Linked List.playground/Contents.swift
@@ -25,18 +25,49 @@ import UIKit
 //The number of nodes in the list is the range [0, 5000].
 //-5000 <= Node.val <= 5000
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public var val: Int
- *     public var next: ListNode?
- *     public init() { self.val = 0; self.next = nil; }
- *     public init(_ val: Int) { self.val = val; self.next = nil; }
- *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
- * }
- */
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() {
+        self.val = 0
+        self.next = nil
+    }
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+    public init(_ val: Int, _ next: ListNode?) {
+        self.val = val
+        self.next = next
+    }
+}
+
+//class Solution {
+//    func reverseList(_ head: ListNode?) -> ListNode? {
+//        guard let head = head, let next = head.next else {
+//            return head
+//        }
+//        
+//        let newHead = reverseList(head.next)
+//        next.next = head
+//        head.next = nil
+//        return newHead
+//    }
+//}
+
 class Solution {
     func reverseList(_ head: ListNode?) -> ListNode? {
+        var prev: ListNode?
+        var curr: ListNode? = head
         
+        while curr != nil {
+            let next = curr?.next
+            curr?.next = prev
+            
+            prev = curr
+            curr = next
+        }
+        
+        return prev
     }
 }
