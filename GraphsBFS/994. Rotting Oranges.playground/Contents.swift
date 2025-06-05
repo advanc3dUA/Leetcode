481e2ab104f2c3f970bc7369cc34a89112363262
Yuriy Gudimov <gudimovyuriy@gmail.com>
2025-06-05 10:38:39 +0200

    Add solution for 994. Rotting Oranges

--- GraphsBFS/994. Rotting Oranges.playground/Contents.swift
+++ GraphsBFS/994. Rotting Oranges.playground/Contents.swift
@@ -9,7 +9,7 @@ import UIKit
 //
 //Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
 //
-// 
+//
 //
 //Example 1:
 //
@@ -29,6 +29,46 @@ import UIKit
 
 class Solution {
     func orangesRotting(_ grid: [[Int]]) -> Int {
+        var queue = [(Int, Int)]()
+        var minutes = 0
+        var fresh = 0
+        let directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        var grid = grid
         
+        for i in 0..<grid.count {
+            for j in 0..<grid[0].count {
+                if grid[i][j] == 2 {
+                    queue.append((i, j))
+                } else if grid[i][j] == 1 {
+                    fresh += 1
+                }
+            }
+        }
+        
+        while !queue.isEmpty && fresh > 0 {
+            let size = queue.count
+            for _ in 0..<size {
+                let (r, c) = queue.removeFirst()
+                for (dr, dc) in directions {
+                    let nr = r + dr
+                    let nc = c + dc
+                    if nr >= 0 && nr < grid.count && nc >= 0 && nc < grid[0].count && grid[nr][nc] == 1 {
+                        grid[nr][nc] = 2
+                        fresh -= 1
+                        queue.append((nr, nc))
+                    }
+                }
+            }
+            minutes += 1
+        }
+        
+        return fresh == 0 ? minutes : -1
     }
 }
+
+let solution = Solution()
+print(solution.orangesRotting([
+    [2,1,1],
+    [1,1,0],
+    [0,1,1]
+]))
