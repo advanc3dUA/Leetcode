39d54f330bcebaca55e83fb414ff0548371a712d
Yuriy Gudimov <gudimovyuriy@gmail.com>
2025-06-08 10:41:05 +0200

    Add solution for 215. Kth Largest Element in an Array

--- HeapPriorityQueue/215. Kth Largest Element in an Array.playground/Contents.swift
+++ HeapPriorityQueue/215. Kth Largest Element in an Array.playground/Contents.swift
@@ -23,8 +23,46 @@ import UIKit
 //1 <= k <= nums.length <= 105
 //-104 <= nums[i] <= 104
 
+//class Solution {
+//    func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
+//        var heap: [Int] = []
+//        
+//        for num in nums {
+//            var i = 0
+//            while i < heap.count && heap[i] < num {
+//                i += 1
+//            }
+//            heap.insert(num, at: i)
+//        }
+//        return heap[0]
+//    }
+//}
+
 class Solution {
     func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
-        
+        var heap = Array<Int>()
+        for num in nums {
+            if heap.count < k {
+                var i = 0
+                while i < heap.count && heap[i] < num {
+                    i += 1
+                }
+                heap.insert(num, at: i)
+            } else {
+                if num > heap[0] {
+                    var i = 0
+                    while i < heap.count && heap[i] < num {
+                        i += 1
+                    }
+                    heap.insert(num, at: i)
+                    heap.removeFirst()
+                }
+            }
+        }
+        return heap[0]
     }
 }
+
+
+let solution = Solution()
+print(solution.findKthLargest([3,2,1,5,6,4], 2))
