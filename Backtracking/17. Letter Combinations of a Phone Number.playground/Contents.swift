c0d2059b668e2c0a261829ad28cc2cac85e4b539
Yuriy Gudimov <gudimovyuriy@gmail.com>
2025-06-28 11:04:32 +0200

    Add solution for 17. Letter Combinations of a Phone Number

--- Backtracking/17. Letter Combinations of a Phone Number.playground/Contents.swift
+++ Backtracking/17. Letter Combinations of a Phone Number.playground/Contents.swift
@@ -25,6 +25,39 @@ import UIKit
 
 class Solution {
     func letterCombinations(_ digits: String) -> [String] {
+        guard !digits.isEmpty else { return [] }
         
+        let mapping: [Character: [Character]] = [
+          "2": ["a","b","c"], "3": ["d","e","f"],
+          "4": ["g","h","i"], "5": ["j","k","l"],
+          "6": ["m","n","o"], "7": ["p","q","r","s"],
+          "8": ["t","u","v"], "9": ["w","x","y","z"]
+        ]
+        
+        var result = [String]()
+        var path = [Character]()
+        let chars = Array(digits)
+        
+        func dfs(_ index: Int) {
+            if index == chars.count {
+                result.append(String(path))
+                return
+            }
+            let digit = chars[index]
+            guard let letters = mapping[digit] else { return }
+            for letter in letters {
+                path.append(letter)
+                dfs(index + 1)
+                print(path)
+                path.removeLast()
+                print(path)
+            }
+        }
+        
+        dfs(0)
+        return result
     }
 }
+
+let solution = Solution()
+print(solution.letterCombinations("23"))
